5c58f68f80c173c84aadcd219fbeb232baa7cfbd
Ekow Yawson <ekowyawson71@gmail.com>
2023-12-15 16:38:55 -0500

    Undid changes due to issues when deployed to netlify.

--- src/components/BestBooks/EditBookModal.jsx
+++ src/components/BestBooks/EditBookModal.jsx
@@ -1,5 +1,5 @@
 // BookFormModal.js
-import useState from 'react';
+import React, { useState } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 
 const BookFormModal = ({ show, handleCloseEditModal, handleEditBook, id, bookTitle, bookDescription, bookStatus }) => {
